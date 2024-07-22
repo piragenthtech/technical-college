f1dd5a072cbbd4a6f21ca6243a5f910f9c2b63a3
piragenthtech <piragenthtech@gmail.com>
2024-07-22 14:17:46 +0530

    Update App.jsx

--- jerinsir/tech-college-system/src/App.jsx
+++ jerinsir/tech-college-system/src/App.jsx
@@ -6,12 +6,14 @@ import Inputform from "./components/Students/Inputform";
 function App() {
   return (
     <>
+    
       <Router>
         <Routes>
           <Route path="/" element={<Header />}></Route>
-          <Route path="/students" element={<Inputform />}></Route>
+          {/* <Route path="/students" element={<Inputform />}></Route> */}
         </Routes>
       </Router>
+      <Inputform />
     </>
   );
 }
