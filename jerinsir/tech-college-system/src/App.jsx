c5c583e774d1d87b1e197dcc6e63a298c1d4853a
piragenthtech <piragenthtech@gmail.com>
2024-07-22 12:10:11 +0530

    student

--- jerinsir/tech-college-system/src/App.jsx
+++ jerinsir/tech-college-system/src/App.jsx
@@ -1,35 +1,17 @@
-import Card from "./Card";
-import Button from "./Button";
-import Students from "./Students";
-import UserGretting from "./UserGretting";
-import List from "./List";
-import MyComponent from "./MyComponent.jsx";
+import * as React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Inputform from "./components/Students/Inputform";
 
 function App() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "orange", calories: 45 },
-    { id: 3, name: "banana", calories: 105 },
-  ];
-
-  const vegetables = [
-    { id: 4, name: "potatoes", calories: 935 },
-    { id: 5, name: "carrots", calories: 452 },
-    { id: 6, name: "celery", calories: 1053 },
-  ];
-
   return (
     <>
-      <Card />
-      <Button />
-      <Students name="piragenth" age={21} isStudent={true} />
-      <Students name="satkunananthan" age={55} isStudent={false} />
-      <Students />
-      <MyComponent />
-      <List items={fruits} category="Fruits" />
-      <List items={vegetables} category="vegetables" />
-
-      <UserGretting isLoggedin={true} username="piragenth" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header />}></Route>
+          <Route path="students" element={<Inputform />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ jerinsir/tech-college-system/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <div className="Headerhead">
+      <a className="Students" href="students">
+        Students
+      </a>
+      <h1 className="Headingheder">Technical College System</h1>
+      <a className="Computers" href="computers">
+        Computers
+      </a>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default Header;
