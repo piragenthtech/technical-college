d8a59ff9eedda214dd841dfff60337b9de61b91a
piragenthtech <piragenthtech@gmail.com>
2024-08-07 08:49:22 +0530

    sdfg

--- PushpaSir/react-app/src/App.js
+++ PushpaSir/react-app/src/App.js
@@ -1,10 +1,18 @@
+<<<<<<< Updated upstream
 // import logo from './logo.svg';
 import "./App.css";
+=======
+// import "./App.css";
+>>>>>>> Stashed changes
 import NavBar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <NavBar />
+=======
+      <NavBar></NavBar>
+>>>>>>> Stashed changes
     </div>
   );
 }
