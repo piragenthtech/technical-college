40a2d4dbae49a39ab71efd6f5c4a0dce94fa3e65
piragenthtech <piragenthtech@gmail.com>
2024-08-12 13:34:27 +0530

    nbvhjb

--- PushpaSir/react-app/src/App.js
+++ PushpaSir/react-app/src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./components/NavBar";
 import Counter from "./components/Counter";
 import Forms from "./components/Forms";
+import Image_Carousel from "./components/Image_Carousel";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Counter />
       <Forms />
+      <Image_Carousel />
     </div>
   );
 }
---
+++ PushpaSir/react-app/src/components/Image_Carousel.js
@@ -0,0 +1,41 @@
+import Carousel from "react-bootstrap/Carousel";
+import porshce_2023_911 from "./images/2023-porsche-911-gt3-rs.jpg";
+import porshce_911_gt3rs_1 from "./images/porsche-911-gt3-rs-2022.jpg";
+import porshce_911_gt3rs_2 from "./images/porsche-911-gt3-rs-2022-2.jpg";
+export default function Image_Carousel() {
+  return (
+    <div
+      style={{
+        display: "block",
+        justifyContent: "center",
+        width: "90%",
+        padding: 30,
+      }}
+    >
+      <h4></h4>
+      <Carousel>
+        <Carousel.Item interval={1500}>
+          <img className="" src={porshce_2023_911} />
+          <Carousel.Caption>
+            <h3>Porsche </h3>
+            <p>GT3 Rs 2023</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img className="" src={porshce_911_gt3rs_1} />
+          <Carousel.Caption>
+            <h3>Porsche </h3>
+            <p>GT3 Rs 2022</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img className="" src={porshce_911_gt3rs_2} />
+          <Carousel.Caption>
+            <h3>Porsche </h3>
+            <p>GT3 Rs 2022</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
