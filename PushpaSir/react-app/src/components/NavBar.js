7b757480964cbd904e8e44b9d597e34e8116ff83
piragenthtech <piragenthtech@gmail.com>
2024-08-15 15:00:27 +0530

    Update NavBar.js

--- PushpaSir/react-app/src/components/NavBar.js
+++ PushpaSir/react-app/src/components/NavBar.js
@@ -1,21 +1,25 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <>
-      <Navbar bg="primary" data-bs-theme="dark">
+      <Navbar expand="lg" bg="primary" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="/">Piragenth</Navbar.Brand>
-
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/departments">departments</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">contact</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/departments">departments</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/contact">contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
